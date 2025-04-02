f1aa8ab7bd5d993fe45d7b5d543c98b8797e023d
YUVRAJSINGHOO7 <159637564+YUVRAJSINGHOO7@users.noreply.github.com>
2025-04-02 22:32:38 +0530

    Update route.js

--- app/api/product/route.js
+++ app/api/product/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   const uri =
-    "mongodb+srv://YuvrajSingh:r9ulZ8YUX5fjk9Hy@stock.ee6nd.mongodb.net/";
+    "mongodb+srv://Yuvraj--SinghStock:Ktks8f93InNhLG6BwwcNpH@cluster0.djkqg.mongodb.net/";
 
   const client = new MongoClient(uri);
 
